817edef70f35489cfc7a07a76838514c88563b1a
Reynaldo Fang <reynaldofang@hotmail.com>
2023-07-13 22:19:00 +0700

    fix carousel, fix sec detail, add new section about and add contact us section

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const carouselContainer = document.querySelector('.carousel-container');
-const indicators = document.querySelectorAll('.indicator');
-const prevArrow = document.querySelector('.carousel-arrow.prev');
-const nextArrow = document.querySelector('.carousel-arrow.next');
+const carouselContainer = document.querySelector(".carousel-container");
+const indicators = document.querySelectorAll(".indicator");
+const prevArrow = document.querySelector(".carousel-arrow.prev");
+const nextArrow = document.querySelector(".carousel-arrow.next");
 
 let currentIndex = 0;
 
@@ -12,9 +12,9 @@ function goToSlide(index) {
 function setActiveIndicator(index) {
   indicators.forEach((indicator, i) => {
     if (i === index) {
-      indicator.classList.add('active');
+      indicator.classList.add("active");
     } else {
-      indicator.classList.remove('active');
+      indicator.classList.remove("active");
     }
   });
 }
@@ -32,16 +32,48 @@ function goToNextSlide() {
 }
 
 indicators.forEach((indicator, index) => {
-  indicator.addEventListener('click', () => {
+  indicator.addEventListener("click", () => {
     currentIndex = index;
     goToSlide(currentIndex);
     setActiveIndicator(currentIndex);
   });
 });
 
-prevArrow.addEventListener('click', goToPrevSlide);
-nextArrow.addEventListener('click', goToNextSlide);
+prevArrow.addEventListener("click", goToPrevSlide);
+nextArrow.addEventListener("click", goToNextSlide);
 
 setActiveIndicator(currentIndex);
 
+setInterval(() => {
+  currentIndex = (currentIndex + 1) % indicators.length;
+  goToSlide(currentIndex);
+  setActiveIndicator(currentIndex);
+}, 5000);
 
+// Dapatkan semua tautan navigasi
+const navLinks = document.querySelectorAll(".nav_link");
+
+// Pasang pendengar acara klik pada setiap tautan navigasi
+navLinks.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault(); // Mencegah perilaku loncat bawaan dari tautan anchor
+
+    // Dapatkan ID bagian tujuan dari atribut href
+    const targetId = link.getAttribute("href");
+
+    // Gunakan ID bagian tujuan untuk memilih elemen bagian yang sesuai
+    const targetSection = document.querySelector(targetId);
+
+    // Hitung tinggi header yang tetap
+    const headerHeight = document.querySelector("header").offsetHeight;
+
+    // Hitung posisi scroll untuk bagian tujuan, dengan mempertimbangkan tinggi header
+    const offsetTop = targetSection.offsetTop - headerHeight;
+
+    // Gulir ke bagian tujuan secara halus
+    window.scrollTo({
+      top: offsetTop,
+      behavior: "smooth",
+    });
+  });
+});
